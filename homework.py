1eb55b52b1710c10ffbacb4f28d55006edb6d239
karoljurasz <karol.jurasz.uzytkowe@gmail.com>
2024-11-18 14:35:58 +0100

    Adjusted 6 markers calibration to work on all images at once'

--- homework.py
+++ homework.py
@@ -3,26 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def find_aruco_tags(images):
-    # Initialize the dictionary and parameters for ArUcO detection
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_16h5)
-    parameters = cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-
-    all_corners = []
-    all_ids = []
-
-    for image in images:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        corners, ids, _ = detector.detectMarkers(gray)
-
-        if ids is not None:
-            all_corners.append(corners)
-            all_ids.append(ids)
-
-    return all_corners, all_ids
-
-
 def object_points(width, spacing):
     objpts = []
     for j in range(3):
@@ -47,7 +27,6 @@ def object_points(width, spacing):
 
 def sort_corners(corners, ids):
     cos = np.argsort(ids.flatten()).astype(int)
-    print(f"cos: {cos}")
     sorted_corners = [
         corners[cos[0]],
         corners[cos[2]],
@@ -60,56 +39,61 @@ def sort_corners(corners, ids):
 
 
 def undistort_image(image, cameraMatrix, distCoeffs):
-    # Get image dimensions
     h, w = image.shape[:2]
 
-    # Optimize the camera matrix based on a balance between field of view and no pixel loss
     new_camera_matrix, _ = cv2.getOptimalNewCameraMatrix(
         cameraMatrix, distCoeffs, (w, h), alpha=1
     )
 
-    # Compute the undistortion and rectification transformation map
-    R = np.eye(3)  # No additional rotation #+0.1 rotate
+    R = np.eye(3)
     mapx, mapy = cv2.initUndistortRectifyMap(
         cameraMatrix, distCoeffs, R, new_camera_matrix, (w, h), m1type=cv2.CV_32FC1
     )
 
-    # Apply the remap to get the undistorted image
     undistorted_image = cv2.remap(image, mapx, mapy, interpolation=cv2.INTER_LINEAR)
 
-    # Crop the image using the ROI if desired (optional)
-    # x, y, w, h = roi
-    # undistorted_image = undistorted_image[y:y+h, x:x+w]
-
     return undistorted_image
+
+
+def calibrating(calibration_images, tag_size, spacing):
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_16h5)
+    parameters = cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    imgptsf = []
+    objptsf = []
+    for img in calibration_images:
+        # Find ArUcO tags
+        img = cv2.imread("calibration\img1.png")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = detector.detectMarkers(gray)
+        corners = sort_corners(corners, ids)
+        objpts = object_points(tag_size, spacing)
+        shape = (img.shape[1], img.shape[0])
+        objptsf.append(objpts)
+        imgptsf.append(corners)
+    _, cameraMatrix, distCoeffs, _, _ = cv2.calibrateCamera(
+        objpts, corners, shape, None, None
+    )
+    return cameraMatrix, distCoeffs
 
 
 def main():
     # Load your images here
     calibration_images = [cv2.imread(f"calibration\img{i}.png") for i in range(1, 29)]
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_16h5)
-    parameters = cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-    # Find ArUcO tags
-    img = cv2.imread("calibration\img1.png")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    corners, ids, _ = detector.detectMarkers(gray)
-    print(ids)
-    print()
     tag_size = 1.68
     spacing = 0.70
-    corners = sort_corners(corners, ids)
-    objpts = object_points(tag_size, spacing)
-    shape = (img.shape[1], img.shape[0])
 
-    ret, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(
-        objpts, corners, shape, None, None
+    cameraMatrix, distCoeffs = calibrating(
+        calibration_images=calibration_images, tag_size=tag_size, spacing=spacing
     )
 
-    undistorted_image = undistort_image(img, cameraMatrix, distCoeffs)
-    cv2.imshow("original image", img)
-    cv2.imshow("undistorted image", undistorted_image)
-    cv2.waitKey(0)
+    print(cameraMatrix)
+    print(distCoeffs)
+
+    # undistorted_image = undistort_image(img, cameraMatrix, distCoeffs)
+    # cv2.imshow("original image", img)
+    # cv2.imshow("undistorted image", undistorted_image)
+    # cv2.waitKey(0)
     # for img in images:
 
 
